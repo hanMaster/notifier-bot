53cc2a7f4d40b3b9f9638f7acbd4bc525f9ab8e9
hanmaster <w54661c@gmail.com>
2025-03-25 08:43:31 +1000

    bot refactored

---
+++ src/bot_interface.rs
@@ -0,0 +1,319 @@
+use crate::config::config;
+use crate::model::deal::{get_house_numbers, get_object_numbers, prepare_response};
+use crate::model::sync::sync;
+use crate::{HandlerResult, MyDialogue};
+use log::info;
+use teloxide::dispatching::dialogue::InMemStorage;
+use teloxide::dispatching::{dialogue, DpHandlerDescription};
+use teloxide::dptree::case;
+use teloxide::macros::BotCommands;
+use teloxide::prelude::*;
+use teloxide::types::{KeyboardButton, KeyboardMarkup, KeyboardRemove, ReplyMarkup};
+
+const PROJECTS: [&str; 2] = ["DNS Сити", "ЖК Формат"];
+const OBJECT_TYPES: [&str; 2] = ["Квартиры", "Кладовки"];
+#[derive(Clone, Default)]
+pub enum State {
+    #[default]
+    Start,
+    ChooseProject,
+    ChooseObjectType {
+        project: String,
+    },
+    ChooseHouseNumber {
+        project: String,
+        object_type: String,
+    },
+    ChooseObjectNumber {
+        project: String,
+        object_type: String,
+        house: i32,
+    },
+    ChooseAnswer {
+        project: String,
+        object_type: String,
+        house: i32,
+    },
+}
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase")]
+pub enum BotCommand {
+    /// Информация по объекту
+    Start,
+    /// Запрос данных в AmoCRM
+    Sync,
+}
+
+pub fn bot_handler<'a>() -> Handler<'static, DependencyMap, HandlerResult, DpHandlerDescription> {
+    dialogue::enter::<Update, InMemStorage<State>, State, _>()
+        .branch(
+            Update::filter_message()
+                .filter_command::<BotCommand>()
+                .branch(case![BotCommand::Sync].endpoint(sync_handler))
+                .branch(case![BotCommand::Start].endpoint(start)),
+        )
+        .branch(
+            Update::filter_message()
+                .branch(case![State::ChooseProject].endpoint(receive_project_name))
+                .branch(case![State::ChooseObjectType { project }].endpoint(receive_object_type))
+                .branch(
+                    case![State::ChooseHouseNumber {
+                        project,
+                        object_type
+                    }]
+                    .endpoint(receive_house_number),
+                )
+                .branch(
+                    case![State::ChooseObjectNumber {
+                        project,
+                        object_type,
+                        house,
+                    }]
+                    .endpoint(receive_object_number),
+                ),
+        )
+}
+
+fn make_kbd(step: i32) -> KeyboardMarkup {
+    let mut keyboard: Vec<Vec<KeyboardButton>> = vec![];
+
+    let labels = if step == 1 { PROJECTS } else { OBJECT_TYPES };
+
+    for label in labels.chunks(2) {
+        let row = label
+            .iter()
+            .map(|&item| KeyboardButton::new(item.to_owned()))
+            .collect();
+
+        keyboard.push(row);
+    }
+
+    KeyboardMarkup::new(keyboard).resize_keyboard()
+}
+
+async fn make_house_kbd(project: &str, object_type: &str) -> KeyboardMarkup {
+    let mut keyboard: Vec<Vec<KeyboardButton>> = vec![];
+
+    let labels = get_house_numbers(project, object_type).await;
+
+    info!("LABELS {:?}", labels);
+
+    for label in labels.chunks(8) {
+        let row = label
+            .iter()
+            .map(|&item| KeyboardButton::new(item.to_string()))
+            .collect();
+
+        keyboard.push(row);
+    }
+
+    KeyboardMarkup::new(keyboard).resize_keyboard()
+}
+
+async fn sync_handler(bot: Bot, msg: Message) -> HandlerResult {
+    bot.send_message(msg.chat.id, "Начат поиск новых сделок...".to_string())
+        .await?;
+    let results = sync().await;
+    let mut have_no_data = true;
+    for res in results {
+        match res {
+            Ok(data) => {
+                if !data.is_empty() {
+                    have_no_data = false;
+                    for r in data {
+                        bot.send_message(msg.chat.id, r.to_string()).await?;
+                    }
+                }
+            }
+            Err(e) => {
+                let admin_id = ChatId(config().ADMIN_ID);
+                bot.send_message(admin_id, e.to_string()).await?;
+            }
+        }
+    }
+    if have_no_data {
+        bot.send_message(msg.chat.id, "Новых сделок не найдено".to_string())
+            .await?;
+    }
+
+    Ok(())
+}
+
+async fn start(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    if let Some(text) = msg.text() {
+        if text.starts_with("/start") {
+            let keyboard = make_kbd(1);
+            bot.send_message(msg.chat.id, "Выберите проект")
+                .reply_markup(keyboard)
+                .await?;
+            dialogue.update(State::ChooseProject).await?;
+        }
+    }
+    Ok(())
+}
+
+async fn receive_project_name(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    match msg.text() {
+        Some(text) => {
+            if PROJECTS.contains(&text) {
+                if text.eq("ЖК Формат") {
+                    bot.send_message(msg.chat.id, "Нет данных")
+                        .reply_markup(ReplyMarkup::KeyboardRemove(KeyboardRemove::new()))
+                        .await?;
+                    dialogue.exit().await?;
+                } else {
+                    let keyboard = make_kbd(2);
+                    bot.send_message(msg.chat.id, "Квартиры или кладовки?")
+                        .reply_markup(keyboard)
+                        .await?;
+                    dialogue
+                        .update(State::ChooseObjectType {
+                            project: text.into(),
+                        })
+                        .await?;
+                }
+            } else {
+                bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
+                    .await?;
+            }
+        }
+        None => {
+            bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
+                .await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn receive_object_type(
+    bot: Bot,
+    dialogue: MyDialogue,
+    project: String, // Available from `State::ChooseProject`.
+    msg: Message,
+) -> HandlerResult {
+    match msg.text() {
+        Some(object_type) => {
+            if OBJECT_TYPES.contains(&object_type) {
+                let keyboard = make_house_kbd(&project, object_type).await;
+                bot.send_message(msg.chat.id, "Выберите номер дома")
+                    .reply_markup(keyboard)
+                    .await?;
+                dialogue
+                    .update(State::ChooseHouseNumber {
+                        project,
+                        object_type: object_type.into(),
+                    })
+                    .await?;
+            } else {
+                bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
+                    .await?;
+            }
+        }
+        _ => {
+            bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
+                .await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn receive_house_number(
+    bot: Bot,
+    dialogue: MyDialogue,
+    (project, object_type): (String, String), // Available from `State::ChooseObject`.
+    msg: Message,
+) -> HandlerResult {
+    match msg.text().map(|text| text.parse::<i32>()) {
+        Some(Ok(house)) => {
+            let houses = get_house_numbers(&project, &object_type).await;
+            if houses.contains(&house) {
+                let numbers = get_object_numbers(&project, &object_type, house).await;
+                if numbers.is_empty() {
+                    bot.send_message(msg.chat.id, "Объектов не найдено".to_string())
+                        .reply_markup(ReplyMarkup::KeyboardRemove(KeyboardRemove::new()))
+                        .await?;
+                } else {
+                    use std::fmt::Write;
+                    let message = numbers.iter().fold(
+                        "Найдены объекты с номерами:\n".to_string(),
+                        |mut output, b| {
+                            let _ = write!(output, "/{}, ", b);
+                            output
+                        },
+                    );
+                    bot.send_message(msg.chat.id, message)
+                        .reply_markup(ReplyMarkup::KeyboardRemove(KeyboardRemove::new()))
+                        .await?;
+                    if numbers.len() > 1 {
+                        bot.send_message(msg.chat.id, "Выберите номер помещения")
+                            .await?;
+                        dialogue
+                            .update(State::ChooseObjectNumber {
+                                project,
+                                object_type,
+                                house,
+                            })
+                            .await?;
+                    } else {
+                        let number = *numbers.first().unwrap();
+                        let report = prepare_response(&project, &object_type, house, number).await;
+                        bot.send_message(msg.chat.id, report).await?;
+                        bot.send_message(msg.chat.id, "Чтобы начать сначала,\n нажмите /start")
+                            .reply_markup(ReplyMarkup::KeyboardRemove(KeyboardRemove::new()))
+                            .await?;
+                        dialogue.exit().await?;
+                    }
+                };
+            } else {
+                bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
+                    .await?;
+            }
+        }
+        _ => {
+            bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
+                .await?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn receive_object_number(
+    bot: Bot,
+    dialogue: MyDialogue,
+    (project, object_type, house): (String, String, i32), // Available from `State::ChooseHouseNumber`.
+    msg: Message,
+) -> HandlerResult {
+    if let Some(text) = msg.text() {
+        let payload = text.trim_start_matches('/');
+        let payload = if payload.contains('@') {
+            payload.split('@').collect::<Vec<&str>>()[0]
+        } else {
+            payload
+        };
+        match payload.parse::<i32>() {
+            Ok(number) => {
+                let objects = get_object_numbers(&project, &object_type, house).await;
+                if objects.contains(&number) {
+                    let report = prepare_response(&project, &object_type, house, number).await;
+                    bot.send_message(msg.chat.id, report).await?;
+                    if objects.len() == 1 {
+                        bot.send_message(msg.chat.id, "Чтобы начать сначала,\n нажмите /start")
+                            .reply_markup(ReplyMarkup::KeyboardRemove(KeyboardRemove::new()))
+                            .await?;
+                        dialogue.exit().await?;
+                    }
+                }
+            }
+            _ => {
+                bot.send_message(msg.chat.id, "Шаблон: /номер помещения")
+                    .await?;
+            }
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use crate::config::config;
+use crate::bot_interface::{bot_handler, BotCommand, State};
 pub use crate::error::Result;
-use crate::model::deal::{get_house_numbers, get_object_numbers, prepare_response};
-use crate::model::sync::sync;
 use dotenvy::dotenv;
 use log::info;
 use std::error::Error;
-use teloxide::dispatching::dialogue;
 use teloxide::dispatching::dialogue::InMemStorage;
-use teloxide::dptree::{case, deps};
-use teloxide::types::{KeyboardButton, KeyboardMarkup, KeyboardRemove, ReplyMarkup};
+use teloxide::dptree::deps;
 use teloxide::{prelude::*, utils::command::BotCommands};
 
 type MyDialogue = Dialogue<State, InMemStorage<State>>;
@@ -19,33 +15,11 @@ mod error;
 mod model;
 mod adapters;
 mod worker;
+mod bot_interface;
 
-#[derive(Clone, Default)]
-pub enum State {
-    #[default]
-    Start,
-    ChooseProject,
-    ChooseObjectType {
-        project: String,
-    },
-    ChooseHouseNumber {
-        project: String,
-        object_type: String,
-    },
-    ChooseObjectNumber {
-        project: String,
-        object_type: String,
-        house: i32,
-    },
-    ChooseAnswer {
-        project: String,
-        object_type: String,
-        house: i32,
-    },
-}
 
-const PROJECTS: [&str; 2] = ["DNS Сити", "ЖК Формат"];
-const OBJECT_TYPES: [&str; 2] = ["Квартиры", "Кладовки"];
+
+
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -55,7 +29,7 @@ async fn main() -> Result<()> {
     info!("Starting DKP bot...");
 
     let bot = Bot::from_env();
-    bot.set_my_commands(Command::bot_commands())
+    bot.set_my_commands(BotCommand::bot_commands())
         .await
         .expect("Failed to set bot commands");
 
@@ -63,35 +37,7 @@ async fn main() -> Result<()> {
 
     worker::do_work(cloned_bot);
 
-    let handler = dialogue::enter::<Update, InMemStorage<State>, State, _>()
-        .branch(
-            Update::filter_message()
-                .filter_command::<Command>()
-                .branch(case![Command::Sync].endpoint(sync_handler))
-                .branch(case![Command::Start].endpoint(start)),
-        )
-        .branch(
-            Update::filter_message()
-                .branch(case![State::ChooseProject].endpoint(receive_project_name))
-                .branch(case![State::ChooseObjectType { project }].endpoint(receive_object_type))
-                .branch(
-                    case![State::ChooseHouseNumber {
-                        project,
-                        object_type
-                    }]
-                    .endpoint(receive_house_number),
-                )
-                .branch(
-                    case![State::ChooseObjectNumber {
-                        project,
-                        object_type,
-                        house,
-                    }]
-                    .endpoint(receive_object_number),
-                ),
-        );
-
-    Dispatcher::builder(bot, handler)
+    Dispatcher::builder(bot, bot_handler())
         .dependencies(deps![InMemStorage::<State>::new()])
         .enable_ctrlc_handler()
         .build()
@@ -101,253 +47,11 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-#[derive(BotCommands, Clone)]
-#[command(rename_rule = "lowercase")]
-enum Command {
-    /// Информация по объекту
-    Start,
-    /// Запрос данных в AmoCRM
-    Sync,
-}
 
-fn make_kbd(step: i32) -> KeyboardMarkup {
-    let mut keyboard: Vec<Vec<KeyboardButton>> = vec![];
 
-    let labels = if step == 1 { PROJECTS } else { OBJECT_TYPES };
 
-    for label in labels.chunks(2) {
-        let row = label
-            .iter()
-            .map(|&item| KeyboardButton::new(item.to_owned()))
-            .collect();
 
-        keyboard.push(row);
-    }
 
-    KeyboardMarkup::new(keyboard).resize_keyboard()
-}
-async fn make_house_kbd(project: &str, object_type: &str) -> KeyboardMarkup {
-    let mut keyboard: Vec<Vec<KeyboardButton>> = vec![];
 
-    let labels = get_house_numbers(project, object_type).await;
 
-    info!("LABELS {:?}", labels);
 
-    for label in labels.chunks(8) {
-        let row = label
-            .iter()
-            .map(|&item| KeyboardButton::new(item.to_string()))
-            .collect();
-
-        keyboard.push(row);
-    }
-
-    KeyboardMarkup::new(keyboard).resize_keyboard()
-}
-
-async fn sync_handler(bot: Bot, msg: Message) -> HandlerResult {
-    bot.send_message(msg.chat.id, "Начат поиск новых сделок...".to_string())
-        .await?;
-    let results = sync().await;
-    let mut have_no_data = true;
-    for res in results {
-        match res {
-            Ok(data) => {
-                if !data.is_empty() {
-                    have_no_data = false;
-                    for r in data {
-                        bot.send_message(msg.chat.id, r.to_string()).await?;
-                    }
-                }
-            }
-            Err(e) => {
-                let admin_id = ChatId(config().ADMIN_ID);
-                bot.send_message(admin_id, e.to_string()).await?;
-            }
-        }
-    }
-    if have_no_data {
-        bot.send_message(msg.chat.id, "Новых сделок не найдено".to_string())
-            .await?;
-    }
-
-    Ok(())
-}
-
-async fn start(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
-    if let Some(text) = msg.text() {
-        if text.starts_with("/start") {
-            let keyboard = make_kbd(1);
-            bot.send_message(msg.chat.id, "Выберите проект")
-                .reply_markup(keyboard)
-                .await?;
-            dialogue.update(State::ChooseProject).await?;
-        }
-    }
-    Ok(())
-}
-
-async fn receive_project_name(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
-    match msg.text() {
-        Some(text) => {
-            if PROJECTS.contains(&text) {
-                if text.eq("ЖК Формат") {
-                    bot.send_message(msg.chat.id, "Нет данных")
-                        .reply_markup(ReplyMarkup::KeyboardRemove(KeyboardRemove::new()))
-                        .await?;
-                    dialogue.exit().await?;
-                } else {
-                    let keyboard = make_kbd(2);
-                    bot.send_message(msg.chat.id, "Квартиры или кладовки?")
-                        .reply_markup(keyboard)
-                        .await?;
-                    dialogue
-                        .update(State::ChooseObjectType {
-                            project: text.into(),
-                        })
-                        .await?;
-                }
-            } else {
-                bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
-                    .await?;
-            }
-        }
-        None => {
-            bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
-                .await?;
-        }
-    }
-
-    Ok(())
-}
-
-async fn receive_object_type(
-    bot: Bot,
-    dialogue: MyDialogue,
-    project: String, // Available from `State::ChooseProject`.
-    msg: Message,
-) -> HandlerResult {
-    match msg.text() {
-        Some(object_type) => {
-            if OBJECT_TYPES.contains(&object_type) {
-                let keyboard = make_house_kbd(&project, object_type).await;
-                bot.send_message(msg.chat.id, "Выберите номер дома")
-                    .reply_markup(keyboard)
-                    .await?;
-                dialogue
-                    .update(State::ChooseHouseNumber {
-                        project,
-                        object_type: object_type.into(),
-                    })
-                    .await?;
-            } else {
-                bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
-                    .await?;
-            }
-        }
-        _ => {
-            bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
-                .await?;
-        }
-    }
-
-    Ok(())
-}
-
-async fn receive_house_number(
-    bot: Bot,
-    dialogue: MyDialogue,
-    (project, object_type): (String, String), // Available from `State::ChooseObject`.
-    msg: Message,
-) -> HandlerResult {
-    match msg.text().map(|text| text.parse::<i32>()) {
-        Some(Ok(house)) => {
-            let houses = get_house_numbers(&project, &object_type).await;
-            if houses.contains(&house) {
-                let numbers = get_object_numbers(&project, &object_type, house).await;
-                if numbers.is_empty() {
-                    bot.send_message(msg.chat.id, "Объектов не найдено".to_string())
-                        .reply_markup(ReplyMarkup::KeyboardRemove(KeyboardRemove::new()))
-                        .await?;
-                } else {
-                    use std::fmt::Write;
-                    let message = numbers.iter().fold(
-                        "Найдены объекты с номерами:\n".to_string(),
-                        |mut output, b| {
-                            let _ = write!(output, "/{}, ", b);
-                            output
-                        },
-                    );
-                    bot.send_message(msg.chat.id, message)
-                        .reply_markup(ReplyMarkup::KeyboardRemove(KeyboardRemove::new()))
-                        .await?;
-                    if numbers.len() > 1 {
-                        bot.send_message(msg.chat.id, "Выберите номер помещения")
-                            .await?;
-                        dialogue
-                            .update(State::ChooseObjectNumber {
-                                project,
-                                object_type,
-                                house,
-                            })
-                            .await?;
-                    } else {
-                        let number = *numbers.first().unwrap();
-                        let report = prepare_response(&project, &object_type, house, number).await;
-                        bot.send_message(msg.chat.id, report).await?;
-                        bot.send_message(msg.chat.id, "Чтобы начать сначала,\n нажмите /start")
-                            .reply_markup(ReplyMarkup::KeyboardRemove(KeyboardRemove::new()))
-                            .await?;
-                        dialogue.exit().await?;
-                    }
-                };
-            } else {
-                bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
-                    .await?;
-            }
-        }
-        _ => {
-            bot.send_message(msg.chat.id, "Сделайте выбор кнопками")
-                .await?;
-        }
-    }
-
-    Ok(())
-}
-
-async fn receive_object_number(
-    bot: Bot,
-    dialogue: MyDialogue,
-    (project, object_type, house): (String, String, i32), // Available from `State::ChooseHouseNumber`.
-    msg: Message,
-) -> HandlerResult {
-    if let Some(text) = msg.text() {
-        let payload = text.trim_start_matches('/');
-        let payload = if payload.contains('@') {
-            payload.split('@').collect::<Vec<&str>>()[0]
-        } else {
-            payload
-        };
-        match payload.parse::<i32>() {
-            Ok(number) => {
-                let objects = get_object_numbers(&project, &object_type, house).await;
-                if objects.contains(&number) {
-                    let report = prepare_response(&project, &object_type, house, number).await;
-                    bot.send_message(msg.chat.id, report).await?;
-                    if objects.len() == 1 {
-                        bot.send_message(msg.chat.id, "Чтобы начать сначала,\n нажмите /start")
-                            .reply_markup(ReplyMarkup::KeyboardRemove(KeyboardRemove::new()))
-                            .await?;
-                        dialogue.exit().await?;
-                    }
-                }
-            }
-            _ => {
-                bot.send_message(msg.chat.id, "Шаблон: /номер помещения")
-                    .await?;
-            }
-        }
-    }
-
-    Ok(())
-}
