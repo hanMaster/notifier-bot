b30ce6af3d0a00da725b095a953f4e650e046383
hanmaster <w54661c@gmail.com>
2025-09-25 16:20:24 +1000

    send_stat refactored

--- src/model/stat.rs
+++ src/model/stat.rs
@@ -6,41 +6,44 @@ use crate::Result;
 
 pub async fn send_stat() -> Result<()> {
     let db = Db::new().await;
-    let deals = db.get_all_undone_deals().await?;
-    let d_cloned = deals.clone();
-    let undone_deals: Vec<DealInfo> = deals.into_iter().map(Into::into).collect();
-    let format_apartments = d_cloned
+    let deals_in_work = db.get_all_undone_deals().await?;
+
+    if deals_in_work.is_empty() {
+        return Ok(());
+    }
+
+    let format_apartments = deals_in_work
         .iter()
         .filter(|d| d.project == PROJECTS[1] && d.object_type == "property")
         .count();
-    let format_pantries = d_cloned
+    let format_pantries = deals_in_work
         .iter()
         .filter(|d| d.project == PROJECTS[1] && d.object_type == "pantry")
         .count();
-    let format_parking = d_cloned
+    let format_parking = deals_in_work
         .iter()
         .filter(|d| d.project == PROJECTS[1] && d.object_type == "parking")
         .count();
-    let city_apartments = d_cloned
+    let city_apartments = deals_in_work
         .iter()
         .filter(|d| d.project == PROJECTS[0] && d.object_type == "property")
         .count();
-    let city_pantries = d_cloned
+    let city_pantries = deals_in_work
         .iter()
         .filter(|d| d.project == PROJECTS[0] && d.object_type == "pantry")
         .count();
-    let stat_numbers: StatNumbers = StatNumbers {
+    let stat_numbers = StatNumbers {
         format_apartments,
         format_pantries,
         format_parking,
         city_apartments,
         city_pantries,
     };
+    let undone_deals: Vec<DealInfo> = deals_in_work.into_iter().map(Into::into).collect();
 
-    if !undone_deals.is_empty() {
-        let email = Email::new();
-        email.stat_notification(undone_deals, stat_numbers).await?;
-    }
+    let email = Email::new();
+    email.stat_notification(undone_deals, stat_numbers).await?;
+
     Ok(())
 }
 
